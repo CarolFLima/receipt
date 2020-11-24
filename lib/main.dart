91c66355bcd961d19cf8e627b479454c68903f75
unknown <carolflima0@gmail.com>
2020-11-24 20:30:44 -0300

    feature: share images

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:image_share/image_share.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.pink,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Gerar recibo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -45,66 +28,176 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _nomeCliente = '';
+  String _enderecoEntrega = '';
+  String _pedido = '';
+  double _valor = 0.0;
+  double _entrada = 0.0;
+  double _restante = 0.0;
+  DateTime _datetime = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
+  void _setRestante() {
+    setState(() {
+      _restante = _valor - _entrada;
+    });
+  }
+
+  @override
+  void initstate() {
+    super.initState();
+  }
+
+  Future<void> _generateReceipt() async {
+    await ImageShare.shareImage(filePath: "assets/images/logo.png");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            // Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            Container(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/images/logo.png',
+                height: 150,
+                width: double.infinity,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                hintText: 'Nome do cliente...',
+                labelText: 'Cliente',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _nomeCliente = value;
+                });
+              },
+            ),
+
+            TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                hintText: 'Endereço de entrega...',
+                labelText: 'Endereço',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _enderecoEntrega = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                filled: true,
+                hintText: 'Lista do pedido...',
+                labelText: 'Pedido',
+              ),
+              onChanged: (value) {
+                _pedido = value;
+              },
+              maxLines: 5,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Valor',
+                hintText: "Valor do pedido... ",
+              ),
+              onChanged: (input) {
+                _valor = double.parse(input);
+              },
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Entrada',
+                hintText: "Primeiro pagamento... ",
+              ),
+              onChanged: (input) {
+                _entrada = double.parse(input);
+                _setRestante();
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(_datetime == null
+                    ? 'Selecione a data...'
+                    : _datetime.toString()),
+                FlatButton(
+                  child: Text('Data'),
+                  onPressed: () {
+                    showDatePicker(
+                            context: context,
+                            initialDate:
+                                _datetime == null ? DateTime.now() : _datetime,
+                            firstDate: DateTime(2001),
+                            lastDate: DateTime(2021))
+                        .then((date) {
+                      setState(() {
+                        _datetime = date;
+                      });
+                    });
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(_timeOfDay == null
+                    ? 'Selecione a hora...'
+                    : _timeOfDay.toString()),
+                FlatButton(
+                  child: Text('Hora'),
+                  onPressed: () {
+                    showTimePicker(
+                      context: context,
+                      initialTime:
+                          _timeOfDay == null ? TimeOfDay.now() : _timeOfDay,
+                    ).then((time) {
+                      setState(() {
+                        _timeOfDay = time;
+                      });
+                    });
+                  },
+                ),
+              ],
             ),
             Text(
-              '$_counter',
+              'Resta: $_restante',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: () => _generateReceipt(),
+        child: Icon(Icons.share),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
